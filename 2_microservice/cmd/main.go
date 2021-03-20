bc60d581bcd58624f23a2aecd168f01989a09c21
ivan <ivan@localhost.localdomain>
2021-03-20 19:45:42 +0700

    update main.go

--- 2_microservice/cmd/main.go
+++ 2_microservice/cmd/main.go
@@ -1,8 +1,13 @@
-package cmd
+package main
 
 import (
+	"IvanFerdino/bibit-golang-test/commons"
 	"IvanFerdino/bibit-golang-test/internal/model"
+	"IvanFerdino/bibit-golang-test/internal/server/rest_server"
 	"IvanFerdino/bibit-golang-test/internal/service"
+	"fmt"
+	"os"
+	"sync"
 )
 
 const (
@@ -16,6 +21,7 @@ const (
 )
 
 func main() {
+	commons.LogInfo(fmt.Sprintf("in this case i will run both server from single binary/main.go.\n but it can be splitted into two main.go. and run both binary (rest server and grpc server)"))
 	movieSvc,db:=service.InitService(&model.InitModel{
 		ApiUrl:     APIURL,
 		ApiKey:     APIKEY,
@@ -27,5 +33,21 @@ func main() {
 	})
 	defer db.Close()
 
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
 
+
+	server:= rest_server.New(movieSvc,"8320")
+	go func() {
+		err := server.Run()
+		if err != nil {
+			commons.LogError(fmt.Sprintf("%v",err))
+			wg.Done()
+			os.Exit(1) //exit this service / all goroutine will be killed too. to make sure both rest and grpc server are running.
+		}
+		commons.LogError(fmt.Sprintf("Server Echo Shutdown"))
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
