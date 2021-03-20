ff6f23e117825feeab9bcf44a692f77f75dd4893
ivan <ivan@localhost.localdomain>
2021-03-20 22:19:05 +0700

    answer to question 3

---
+++ 3_CodeRefactoring/go.mod
@@ -0,0 +1,3 @@
+module IvanFerdino/bibit-golang-test/coderefactoring
+
+go 1.16
---
+++ 3_CodeRefactoring/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(findFirstStringInBracket("abc(defgh)adsa"))
+	//fmt.Println(findFirstStringInBracket("ad)dasd(dasdsad"))
+	//fmt.Println(findFirstStringInBracket("(adasd(dasdas))"))
+	//fmt.Println(findFirstStringInBracketOriginal("(adasd(dasdas))"))
+}
+
+//findFirstStringInBracket is refactored code
+func findFirstStringInBracket(str string) string {
+	if len(str) > 0 {
+		indexOpeningBracket := strings.Index(str,"(")
+		indexClosingBracket := strings.Index(str,")")
+
+		if (indexClosingBracket >indexOpeningBracket) && (indexOpeningBracket >=0 && indexClosingBracket >= 0) {
+			runes := []rune(str)
+			return string(runes[indexOpeningBracket+1:indexClosingBracket])
+		}
+	}
+	return ""
+}
+
+
+
+
+
+
+
+//findFirstStringInBracketOriginal is original code from the test question
+func findFirstStringInBracketOriginal(str string) string {
+	if (len(str) > 0) {
+		indexFirstBracketFound := strings.Index(str,"(")
+		if indexFirstBracketFound >= 0 {
+			runes := []rune(str)
+			wordsAfterFirstBracket := string(runes[indexFirstBracketFound:len(str)])
+			indexClosingBracketFound := strings.Index(wordsAfterFirstBracket,")")
+			if indexClosingBracketFound >= 0 {
+				runes := []rune(wordsAfterFirstBracket)
+				return string(runes[1:indexClosingBracketFound-1])
+			}else{
+				return ""
+			}
+		}else{
+			return ""
+		}
+	}else{
+		return ""
+	}
+	return ""
+}
